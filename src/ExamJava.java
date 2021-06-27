2e596e822854cd0a719e2bc6055241d0eb867086
minglegithub <turingx3@gmail.com>
2021-06-27 19:20:07 +0900

    1st commit

--- src/ExamJava.java
+++ src/ExamJava.java
@@ -3,7 +3,7 @@ public class ExamJava {
     public static void main(String[] args) {
         int[] score = {70, 80, 90, 100};
 
-        int[] res = new Calc().Hello(score);
+        Integer[] res = new Calc().HelloFun(score);
         System.out.println("총점은 " + res[0] + "이고 평균은 " + res[1] + "입니다.");
         System.out.printf("총점은 %d이고 평균은 %d입니다.",res[0],res[1]);
 
@@ -17,8 +17,8 @@ public class ExamJava {
 
 
 class Calc {
-    int[] Hello(int[] score) {
-        int[] tmp = {0, 0};
+    Integer[] HelloFun(int[] score) {
+        Integer[] tmp = {0, 0};
         for(int i : score){
             tmp[0] += i;
         }
---
+++ src/Input.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+public class Input {
+    public static void main(String[] args) throws IOException{
+        int input = System.in.read();
+        System.out.println(input);
+    }
+
+}
