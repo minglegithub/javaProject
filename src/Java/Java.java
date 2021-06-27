24255a6f8d59f44b4064a63f88845026629a9134
minglegithub <turingx3@gmail.com>
2021-06-27 21:45:53 +0900

    1st commit

--- src/Java/Java.java
+++ src/Java/Java.java
@@ -3,22 +3,43 @@ package Java;
 public class Java {
     public static void main(String[] args) {
         String[] data = {"식초", "식용유"};
-        String[] res = Algo.box(data);
-        System.out.println(res[0] + "," + res[1]);
-        System.out.printf("%s,%s",res[0],res[1]);
+        int[] score = {70, 80, 90, 100};
+        Algo res = new Algo(data, score);
+        res.box();
+        System.out.println(res.d[0] + "," + res.d[1]);
+        res.HelloFun();
+        System.out.println(res.tmp[0] + "," + res.tmp[1]);
+
 
     }
 
 }
 
 class Algo {
-    static String[] box(String[] d) {
+    String[] d;
+    int[] score;
+    int[] tmp = new int[2];
+    Algo(String[] d, int[] score) {
+        this.d = d;
+        this.score = score;
+    }
+    String[] box() {
 
 
         String temp = d[0];
         d[0] = d[1];
         d[1] = temp;
         return d;
+
+    }
+
+    int[] HelloFun() {
+
+        for(int i : score){
+            tmp[0] += i;
+        }
+        tmp[1] = tmp[0]/score.length;
+        return tmp;
 
     }
 }
